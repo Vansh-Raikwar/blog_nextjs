9ef8250f1d7caa86c6e2ce3bfe3c2fd90c0f3630
Vansh-Raikwar <vanshraikwar880@gmail.com>
2026-02-26 21:48:10 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+
+import Image from "next/image";
+import Footer from "./components/Footer/Footer.jsx"
+import Navbar from "../app/components/Navbar/Navbar.jsx"
+
+
+export default function Home() {
+  return (
+    <div>
+      main content
+    </div>
+
+  );
+}
